b8f0d387bb214f2354b48adf10e2695d26f44dd1
Saldanha <joaoaosaldanha@gmail.com>
2024-01-12 20:14:43 -0300

    ci/cd

--- functions/constants.py
+++ functions/constants.py
@@ -3,8 +3,7 @@ import os
 DB_HOSTNAME = os.environ.get('DB_HOSTNAME')
 DB_USERNAME = os.environ.get('DB_USERNAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
-DB_DATABASE = os.environ.get('DB_DATABASE')
 
-CONNECTION_STRING = f"host={DB_HOSTNAME} user={DB_USERNAME} password={DB_PASSWORD} dbname={DB_DATABASE}"
+CONNECTION_STRING = f"host={DB_HOSTNAME} user={DB_USERNAME} password={DB_PASSWORD} dbname={DB_USERNAME}"
 
 COORDINATES_URL = f"https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name="
