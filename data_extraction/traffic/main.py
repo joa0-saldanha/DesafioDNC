03873048740fc167763690fb272a192838cb0d73
Saldanha <joaoaosaldanha@gmail.com>
2024-01-12 17:25:23 -0300

    update traffic function

--- data_extraction/traffic/main.py
+++ data_extraction/traffic/main.py
@@ -3,50 +3,39 @@ import psycopg2 as psy
 import json
 
 import constants as cons
-    
-def get_routes():
 
+def get_routes():
     print("Getting ROUTES information!")
 
     try:
-        connection = psy.connect(cons.CONNECTION_STRING)
+        with psy.connect(cons.CONNECTION_STRING) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT
+                        r.id AS route_id,
+                        o.latitude AS origin_lat,
+                        o.longitude AS origin_lon,
+                        d.latitude AS dest_lat,
+                        d.longitude AS dest_lon
+                    FROM route r
+                    LEFT JOIN city AS o 
+                        ON r.origin = o.id
+                    LEFT JOIN city AS d 
+                        ON r.destination = d.id;
+                """)
+                routes = [{'id': route[0], 'origin': (route[1], route[2]), 'destination': (route[3], route[4])} for route in cursor.fetchall()]
+
+        return get_traffic(routes)
+
     except Exception as e:
         raise e
 
-    cursor = connection.cursor()
-    cursor.execute(f"""
-                SELECT
-                     r.id            AS route_id
-                    ,o.latitude      AS origin_lat
-                    ,o.longitude     AS origin_lon
-                    ,d.latitude      AS dest_lat
-                    ,d.longitude     AS dest_lon
-                FROM route r
-                LEFT JOIN city AS o 
-                   ON r.origin = o.id
-                LEFT JOIN city AS d 
-                   ON r.destination = d.id;
-                """)
-    
-    routes = [{
-        'id': route[0],
-        'origin': (route[1],route[2]),
-        'destination': (route[3],route[4])
-    } for route in cursor.fetchall()]
-
-    cursor.close()
-    connection.close()
-
-    return get_traffic(routes)
-
 def get_traffic(routes: list):
-
     print("Getting TRAFFIC information!")
-    
+
     for route in routes:
-
         url_call = f"https://api.tomtom.com/routing/1/calculateRoute/{route['origin'][0]},{route['origin'][1]}:{route['destination'][0]},{route['destination'][1]}/json?key={cons.API_KEY}"
-
+        
         try:
             route['info'] = get(url_call).json()['routes'][0]["summary"]
 
@@ -56,42 +45,34 @@ def get_traffic(routes: list):
     return insert_traffic(routes)
 
 def insert_traffic(routes: list):
-
     print("Inserting TRAFFIC information into the DATABASE!")
 
     try:
-        connection = psy.connect(cons.CONNECTION_STRING)
+        with psy.connect(cons.CONNECTION_STRING) as connection:
+            for route in routes:
+                with connection.cursor() as cursor:
+                    cursor.execute("""
+                        INSERT INTO public.traffic
+                        (route, distance_in_meters, departure_time, arrival_time, travel_time_in_seconds, traffic_delay_in_seconds, traffic_distance_in_meters)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    """, (
+                        route['id'],
+                        route['info']['lengthInMeters'],
+                        route['info']['departureTime'],
+                        route['info']['arrivalTime'],
+                        route['info']['travelTimeInSeconds'],
+                        route['info']['trafficDelayInSeconds'],
+                        route['info']['trafficLengthInMeters']
+                    ))
+                connection.commit()
+
+        return 'Ok'
+
     except Exception as e:
         raise e
-    
-    for route in routes:
-
-        cursor = connection.cursor()
-
-        cursor.execute(f"""
-                INSERT INTO "public"."traffic"
-                (route, distance_in_meters, departure_time, arrival_time, travel_time_in_seconds, traffic_delay_in_seconds, traffic_distance_in_meters)
-                VALUES 
-                ({route['id']}, '{route['info']['lengthInMeters']}', '{route['info']['departureTime']}', '{route['info']['arrivalTime']}', {route['info']['travelTimeInSeconds']}, {route['info']['trafficDelayInSeconds']}, {route['info']['trafficLengthInMeters']}) 
-                """)
-        
-        connection.commit()
-        cursor.close()
-
-    connection.close()
-
-    return 'Ok'
 
 def traffic(request):
-
     if request == 'get_traffic':
-
-        return get_routes()    
+        return get_routes()
     else:
-
         return 'Invalid request!'
-
-
-
-
-
