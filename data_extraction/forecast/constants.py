f16d66eb36587e29036fbacb2af53a14589cd7ab
Saldanha <joaoaosaldanha@gmail.com>
2024-01-12 19:33:15 -0300

    .

--- data_extraction/forecast/constants.py
+++ data_extraction/forecast/constants.py
@@ -7,6 +7,5 @@ DB_DATABASE = os.environ.get('DB_DATABASE')
 
 CONNECTION_STRING = f"host={DB_HOSTNAME} user={DB_USERNAME} password={DB_PASSWORD} dbname={DB_DATABASE}"
 
-
 COORDINATES_URL = f"https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name="
 FORECAST_URL = f"https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,relative_humidity_2m,precipitation&timezone=America%2FSao_Paulo&forecast_days=1&"
