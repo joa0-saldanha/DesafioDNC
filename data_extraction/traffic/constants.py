00c02557e236a2e01d938e48753246828ae5bf96
Saldanha <joaoaosaldanha@gmail.com>
2024-01-12 21:25:15 -0300

    .

---
+++ data_extraction/traffic/constants.py
@@ -0,0 +1,9 @@
+import os
+
+DB_HOSTNAME = os.environ.get('DB_HOSTNAME')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+CONNECTION_STRING = f"host={DB_HOSTNAME} user={DB_USERNAME} password={DB_PASSWORD} dbname={DB_USERNAME}"
+
+API_KEY = os.environ.get('API_KEY')
