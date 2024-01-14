287f1127512c32d107061573a8b98fa88727b073
Saldanha <joaoaosaldanha@gmail.com>
2024-01-14 01:13:06 -0300

    update import

--- orchestration/airflow/dags/traffic.py
+++ orchestration/airflow/dags/traffic.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 
 from airflow.models import DAG
 from operators.gcp_functions import CallGoogleCloudFunctionsOperator
-from airflow.providers.google.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.operators.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
 
 
 schema = [
