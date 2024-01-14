687a21030d19e3f17c53278db942fd3ee146fe43
Saldanha <joaoaosaldanha@gmail.com>
2024-01-14 00:19:33 -0300

    update requirements

--- data_extraction/forecast/constants.py
+++ data_extraction/forecast/constants.py
@@ -20,5 +20,4 @@ ROUTES_QUERY = """
             ON r.destination = d.id;
     """
 
-COORDINATES_URL = f"https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name="
 FORECAST_URL = f"https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,relative_humidity_2m,precipitation&timezone=America%2FSao_Paulo&forecast_days=1&"
